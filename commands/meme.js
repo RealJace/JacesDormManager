49346bde63824c31ae7743868c3630872ec37895
RealJace <jacemorphing.roblox@gmail.com>
2023-04-10 14:54:35 +0200

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -8,7 +8,8 @@ export async function execute(interaction) {
 	if (interaction instanceof Eris.CommandInteraction) {
 		
 		const raw_meme_data = await fetch("https://www.reddit.com/r/memes/random/.json");
-		const meme_data = await raw_meme_data.json()
+		const meme_response_data = await raw_meme_data.json()
+		const meme_data = meme_response_data[0].data.children[0].data
 		
 		console.log(meme_data);
 
