0eb9a3a13750af6cd097ac811905cda39d25127c
RealJace <jacemorphing.roblox@gmail.com>
2023-04-10 15:06:50 +0200

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -8,11 +8,26 @@ export async function execute(interaction) {
 	if (interaction instanceof Eris.CommandInteraction) {
 		
 		const raw_meme_data = await fetch("https://www.reddit.com/r/memes/random/.json");
-		const meme_response_data = await raw_meme_data.json()
-		const meme_data = meme_response_data[0].data.children[0].data
-		
-		console.log(meme_data);
+		const meme_response_data = await raw_meme_data.json();
+		const meme_data = meme_response_data[0].data.children[0].data;
 
-		return interaction.createMessage("ive got a meme but im not showing your >:)");
+		const raw_author_data = await fetch(`https://www.reddit.com/user/${meme_data.author}/about.json`);
+		const author_data = await raw_author_data.json();
+		
+		console.log(author_data);
+
+		return interaction.createMessage({
+			embeds: [
+				{
+					title: meme_data.title,
+					url: `https://www.reddit.com${meme_data.permalink}`,
+					image: {url: meme_data.url},
+					footer: {
+						text: meme_data.author,
+
+					}
+				}
+			]
+		});
 	}
 }
