3404364cd2a1126226a0582c0b75fe28e2f50562
RealJace <jacemorphing.roblox@gmail.com>
2023-04-10 14:16:13 +0200

    yet another bruh

--- commands/meme.js
+++ commands/meme.js
@@ -1,11 +1,11 @@
-import Eris from "eris";
+import eris from "eris";
 import fetch from "node-fetch";
 
 export const name = "meme";
 export const description = "giggles and funnies";
 export const options = [];
 export async function execute(interaction) {
-	if (interaction instanceof Eris.CommandInteraction) {
+	if (interaction instanceof eris.CommandInteraction) {
 		
 		let raw_meme_data = await fetch("https://www.reddit.com/r/memes/random/.json");
 		const meme_data = JSON.parse(raw_meme_data);
--- commands/test.js
+++ commands/test.js
@@ -1,16 +1,16 @@
-import Eris from "eris";
+import eris from "eris";
 
 export const name = "test";
 export const description = "Testing command";
 export const options = [
 	{
-		type: Eris.Constants.ApplicationCommandOptionTypes.INTEGER,
+		type: eris.Constants.ApplicationCommandOptionTypes.INTEGER,
 		name: "say",
 		description: "slay",
 	}
 ];
 export async function execute(interaction) {
-	if (interaction instanceof Eris.CommandInteraction) {
+	if (interaction instanceof eris.CommandInteraction) {
 		return interaction.createMessage("hello");
 	}
 };
