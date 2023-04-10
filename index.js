4a30e76feea3c9afba680fc958fb336c292ef698
RealJace <jacemorphing.roblox@gmail.com>
2023-04-10 19:29:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,13 @@ const guildId = "1036643905480970251"; // What guild you want the commands to be
 export async function editUserDb(user) {
 	if (user instanceof Eris.User) {
 		const user_data = await database.get("SELECT * FROM users WHERE id = ?",[user.id]);
+		typeof(user_data);
 		if (typeof(user_data) === undefined) {
 			await database.exec("INSERT INTO users(id) VALUES (?)",[user.id]);
+			console.log("Inserted data into a database");
 		} else {
 			await database.exec("UPDATE users SET id = ?",[user.id]);
+			console.log("Updated data in a database");
 		}
 		const user_data_new = await database.get("SELECT * FROM users WHERE id = ?",[user.id]);
 		console.log(user_data_new);
